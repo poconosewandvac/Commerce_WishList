047b3590336a49c7451c92a73615c38a534e9e0f
Tony <tonyklapatch@hotmail.com>
2018-05-10 20:15:38 -0400

    Skeleton files

---
+++ core/components/commerce_wishlist/docs/readme.txt
@@ -0,0 +1,5 @@
+Wishlist for Commerce
+------------------------
+
+A short description of what Wishlist does or how to set it up.
+
---
+++ core/components/commerce_wishlist/lexicon/en/default.inc.php
@@ -0,0 +1,4 @@
+<?php
+
+$_lang['commerce_wishlist'] = 'Wishlist';
+$_lang['commerce_wishlist.description'] = 'A short description of what Wishlist does.';
