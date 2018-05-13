7355bba30f2ddaf25ead649e16df8d89fbf8b0c9
Tony <tonyklapatch@hotmail.com>
2018-05-13 13:57:08 -0400

    Generate secret, getlists, addlist, init package

--- core/components/commerce_wishlist/model/commerce_wishlist/wishlist.class.php
+++ core/components/commerce_wishlist/model/commerce_wishlist/wishlist.class.php
@@ -10,6 +10,10 @@
  * @license See core/components/commerce_wishlist/docs/license.txt
  */
 class Wishlist {
+    public $modx;
+    public $user;
+    public $commerce;
+    public $config = [];
 
     /**
      * Initialize modX, Commerce, and user
@@ -22,7 +26,7 @@ class Wishlist {
         $this->modx =& $modx;
         $this->user = $config['user'];
 
-        $basePath = $this->modx->getOption('commerce_wishlist.core_path', $config, $this->modx->getOption('core_path').'components/commerce_wishlist/');
+        $corePath = $this->modx->getOption('commerce_wishlist.core_path', $config, $this->modx->getOption('core_path').'components/commerce_wishlist/');
         $assetsUrl = $this->modx->getOption('commerce_wishlist.assets_url', $config, $this->modx->getOption('assets_url').'components/commerce_wishlist/');
         $this->config = array_merge([
             'corePath' => $corePath,
@@ -36,6 +40,9 @@ class Wishlist {
             'jsUrl' => $assetsUrl.'js/',
             'connectorUrl' => $assetsUrl.'connector.php'
         ]);
+        
+        // Add packages
+        $this->modx->addPackage('commerce_wishlist', $this->config['modelPath']);
 
         // Load Commerce
         $commercePath = $this->modx->getOption('commerce.core_path', null, $this->modx->getOption('core_path') . 'components/commerce/') . 'model/commerce/';
@@ -63,7 +70,7 @@ class Wishlist {
             //$this->modx->regClientCSS($this->config['cssUrl'] . 'wishlist.css');
         }
         if ($js) {
-            $this->modx->regClientScript($this->config['jsUrl'] . 'vue.js');
+            $this->modx->regClientScript($this->config['jsUrl'] . 'wishlist.js');
         }
     }
 
@@ -71,28 +78,80 @@ class Wishlist {
      * Add list from submission
      *
      * @param array $values
-     * @return void
+     * @return int|bool list id
      */
     public function addList($values) {
+        $values['secret'] = $this->generateSecret();
         
+        $query = $this->modx->newObject("WishlistList");
+        $query->fromArray($values);
+        $query->save();
+
+        if (!$query) {
+            return false;
+        }
+
+        return $query->get('id');
     }
 
     /**
      * Add item from submission
      *
      * @param array $values
-     * @return void
+     * @return int|bool item id
      */
     public function addItem($values) {
-        
+        $query = $this->modx->newObject("WishlistItem");
+        $query->fromArray($values);
+        $query->save();
+
+        if (!$query) {
+            return false;
+        }
+
+        return $query->get('id');
     }
 
     /** 
      * Get all user lists
      * 
-     * @return string
+     * @param array Override what lists it is looking for
+     * @return collection
      */
-    public function getLists() {
+    public function getLists($where = false) {
+        $query = $this->modx->newQuery("WishlistList");
 
+        if ($where) {
+            $query->fromArray($where);
+        } else {
+            $query->where([
+                'user' => $this->getUser()
+            ]);
+        }
+        
+        return $this->modx->getCollection('WishlistList', $query);
+    }
+
+    /**
+     * Generates secret field to use in URL
+     *
+     * @param int bytes of secret field
+     * @param bool check if it is a duplicate
+     * @return void
+     */
+    public function generateSecret($bytes = 5, $check = true) {
+        $secret = bin2hex(openssl_random_pseudo_bytes($bytes));
+
+        // Check to ensure random generated string has not been used before
+        if ($check) {
+            $query = $this->modx->getObject('WishlistList', ['secret' => $secret]);
+            
+            if ($query) {
+                // Generate a new one if it is being used.
+                $secret = $this->generateSecret($bytes, $check);
+            }
+        }
+
+        return $secret;
     }
 }
